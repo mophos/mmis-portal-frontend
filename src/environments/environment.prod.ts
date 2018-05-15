b22e1a1511d93b1c8437cfd0ec0278cf12d32e89
peemppk <36835770+peemppk@users.noreply.github.com>
2018-05-15 21:28:07 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   apiUrl: '/api/portal',
-  purchasingUrl: '/purchasing',
-  planningUrl: '/planning',
-  inventoryUrl: '/inventory',
-  materialsUrl: '/material',
-  contractsUrl: '/contracts',
-  reportUrl: '/reports',
-  umUrl: '/um',
+  purchasingUrl: '/purchasing/#/',
+  planningUrl: '/planning/#/',
+  inventoryUrl: '/inventory/#/',
+  materialsUrl: '/material/#/',
+  contractsUrl: '/contracts/#/',
+  reportUrl: '/reports/#/',
+  umUrl: '/um/#/',
   umLoginUrl: '/api/um'
 };
